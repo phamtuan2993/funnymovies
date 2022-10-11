8a95fdda93e3929673128f15915b37e20f255c9e
phamtuan2993 <tuan.thieu@employmenthero.com>
2022-10-11 21:01:29 +0700

    Add validation to movie url

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :shared_by, class_name: '::User'
+
+  validates :url, presence: true
+  validates :url, format: { with: URI.regexp }, allow_blank: true
 end
