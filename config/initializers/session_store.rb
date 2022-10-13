b0d36a28359d498e329154969c38358d46a7aa4c
phamtuan2993 <tuan.thieu@employmenthero.com>
2022-10-13 19:51:47 +0700

    Remove session_store as we dont use it

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store,
-  key: '_funny_movies_session',
-  secure: (Rails.env.production? || Rails.env.staging?),
-  httponly: true,
-  same_site: :lax
+Rails.application.config.session_store :disabled
